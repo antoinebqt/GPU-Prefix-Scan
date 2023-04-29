e9fed61b9bf60cdf85ad606a9e560df79caa5b7d
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2023-04-29 16:29:18 +0200

    project gpu

--- Projet_CPU_Scan.py
+++ Projet_CPU_Scan.py
@@ -4,16 +4,16 @@ import numpy as np
 def scanCPU(a):
     n = len(a)
     m = int(np.log2(n))
-    print("Array au début : ", a)
+    #print("Array au début : ", a)
 
     for d in range(0, m):
         for k in range(0, n, pow(2, d + 1)):
             a[k + pow(2, d + 1) - 1] += a[k + pow(2, d) - 1]
 
-    print("Array apres la montée : ", a)
+    #print("Array apres la montée : ", a)
 
     a[n - 1] = 0
-    print("Array apres la mise a 0 : ", a)
+    #print("Array apres la mise a 0 : ", a)
 
     for d in range(m - 1, -1, -1):
         for k in range(0, n, pow(2, d + 1)):
@@ -21,8 +21,10 @@ def scanCPU(a):
             a[k + pow(2, d) - 1] = a[k + pow(2, d + 1) - 1]
             a[k + pow(2, d + 1) - 1] += t
 
-    print("Array apres la descente : ", a)
+    print(a)
 
 
-array = np.array([2, 9, 15, 13, 10, 20, 2, 3], dtype=np.int32)
+array = np.array(
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32],
+    dtype=np.int32)
 scanCPU(array)
--- Projet_CPU_Scan_Em.py
+++ Projet_CPU_Scan_Em.py
@@ -39,7 +39,8 @@ def generate_random_array(size):
     return random_array
 
 
-arrayTD = np.array([2, 3, 4, 6, 1, 2, 3, 4])
+arrayTD = np.array(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32])
 
 scanCPU(arrayTD)
 # scanCPU(generate_random_array(64))
